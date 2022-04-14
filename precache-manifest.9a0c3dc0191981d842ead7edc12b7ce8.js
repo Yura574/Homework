7b9cb55478b0d5732bcb12e70dad5349da50ed36
Yura <yura5742248@mail.ru>
2022-04-14 16:20:30 +0300

    Updates

---
+++ precache-manifest.9a0c3dc0191981d842ead7edc12b7ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629e9198c0f33a6df7c3144d8d214374",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "8d867954d3ce06c5c612",
+    "url": "/homework/static/css/main.e71ce79a.chunk.css"
+  },
+  {
+    "revision": "ae97dcad7e6a8c2f5396",
+    "url": "/homework/static/js/2.9b3c86bc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homework/static/js/2.9b3c86bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d867954d3ce06c5c612",
+    "url": "/homework/static/js/main.38da029d.chunk.js"
+  },
+  {
+    "revision": "8c5bfeabec2c84a40778",
+    "url": "/homework/static/js/runtime-main.f38b6f31.js"
+  },
+  {
+    "revision": "a51c310669f1046b22eba054b894c77d",
+    "url": "/homework/static/media/space.a51c3106.jpg"
+  }
+]);
